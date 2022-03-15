510560f3e9db91e3bfc84d08b65981c96d17741c
RhysMurage <rhysmwangi96@gmail.com>
2022-03-15 18:00:47 -0400

    Add 8-24_hours.c script

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,5 +9,45 @@ void jack_bauer(void);
 
 void jack_bauer(void)
 {
+	int i = 0, k = 0, h = 0;
+
+	for (i = 0; i < 1440; i++)
+	{
+	if (h < 10)
+	{
+		_putchar(0 + '0');
+		_putchar(h + '0');
+	}
+	else
+	{
+		int firstdigit = h / 10;
+		int secondigit = h % 10;
+
+		_putchar(firstdigit + '0');
+		_putchar(secondigit + '0');
+	}
+	_putchar(':');
+	if (k < 10)
+	{
+		_putchar(0 + '0');
+		_putchar(k + '0');
+	}
+	else
+	{
+		int firstdigit = k / 10;
+		int secondigit = k % 10;
+
+		_putchar(firstdigit + '0');
+		_putchar(secondigit + '0');
+	}
+	k++;
+	if (k > 59)
+	{
+		k = 0;
+		h++;
+	}
+	_putchar('\n');
+	}
+}
 
 
