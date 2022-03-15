3d468ada865ab267886b6fff2d7e61b31e263546
RhysMurage <rhysmwangi96@gmail.com>
2022-03-15 17:48:10 -0400

    Add 10-add.c script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add- add 2 integers
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: sum of the 2 integers
+ */
+
+int add(int x, int y);
+
+int add(int x, int y)
+{
+	return (x + y);
+}
