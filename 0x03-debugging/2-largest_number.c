67320e3a65491b7c9e9bb20f85c6da93bff0869e
RhysMurage <rhysmwangi96@gmail.com>
2022-03-17 19:47:19 -0400

    Fix trailing spaces

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,8 +16,8 @@ int largest_number(int a, int b, int c)
 		largest = a;
 	else if ((b > a) && ((a > c) || (a < c)))
 		largest = b;
-	else if ((c>a) && ((a>b)||(a<b)))
-		largest = c;	
+	else if ((c > a) && ((a > b) || (a < b)))
+		largest = c;
 
 	return (largest);
 }
