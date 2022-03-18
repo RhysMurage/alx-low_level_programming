546f8746ab604976bdbc4b67e3d07bfc2c7c9391
RhysMurage <rhysmwangi96@gmail.com>
2022-03-17 22:26:06 -0400

    Add script and update main.h

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- prints the numbers, from 0 to 9 excluding 2 and 4, followed by a new line
+ */
+
+void print_most_numbers(void);
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; (i <= 48) && ((i != 50 || i!= 52)); i++)
+	       _putchar(i);
+	_putchar('\n');
+}
+
