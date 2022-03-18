201b8a20b0656051714837ca44e0fb25fc5d833a
RhysMurage <rhysmwangi96@gmail.com>
2022-03-17 21:27:21 -0400

    Add _isupper.c function

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main- Entry point
+ *
+ * Description: print out characters
+ *
+ * Return: Always (0) successfully
+ * On error, -1 is returned, and errno is set appropriately
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+
+int _isupper(int c);
+
+#endif /* _MAIN_H */
+
