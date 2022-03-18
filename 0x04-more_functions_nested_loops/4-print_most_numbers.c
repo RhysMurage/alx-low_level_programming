0a747e312665e3ed19897e2faf49a372f69ce957
RhysMurage <rhysmwangi96@gmail.com>
2022-03-17 22:42:43 -0400

    Fix indentation

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,10 @@ void print_most_numbers(void)
 	int i;
 
 	for (i = 48; i <= 57; i++)
-	       if (i != 50 && i != 52)
-		       _putchar(i);
+	{
+		if (i != 50 && i != 52)
+		_putchar(i);
+	}
 	_putchar('\n');
 }
 
