fedd85702e8abd5bf90e1ac8aa45ba7233b1ec98
RhysMurage <rhysmwangi96@gmail.com>
2022-03-21 19:55:15 -0400

    Add main.h and 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts- function that prints a string and new line to stdout
+ * @str: string to be printed
+ */
+
+void _puts(char *str);
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+
+}
+
