2e64fce53aef322bbdd2e74939484c8d03628c38
RhysMurage <rhysmwangi96@gmail.com>
2022-04-04 07:33:17 -0400

    Initial commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main- returns the product of 2 numbers
+  * @argc: number of arguments passed
+  * @argv: pointer to array of pointers of strings
+  *
+  * Return: Always 0; 1 otherwise
+  */
+
+int main(int argc, char *argv[])
+{
+	char error[] = "Error";
+	long product;
+	long firstNum;
+	long secondNum;
+
+	if (argc > 1)
+	{
+		firstNum = strtol(argv[1], NULL, 10);
+		secondNum = strtol(argv[2], NULL, 10);
+		product = firstNum * secondNum;
+		printf("%lu\n", product);
+		return (0);
+	}
+	else
+	{
+		printf("%s\n", error);
+		return (1);
+	}
+}
