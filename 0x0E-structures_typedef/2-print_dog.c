684070545d0cc92a85e3b7f65a4460aa8fa3fbd4
RhysMurage <rhysmwangi96@gmail.com>
2022-04-12 14:51:00 -0400

    Add print statement when d is null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,10 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
+	{
+		printf("%s", "(nil)");
 		d = malloc(sizeof(struct dog));
+	}
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s", d->owner);
