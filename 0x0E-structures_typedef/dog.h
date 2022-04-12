2f3d954de80aa1f40a4d2a137933fdfb6dd9ed7a
RhysMurage <rhysmwangi96@gmail.com>
2022-04-12 05:29:27 -0400

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+  * struct dog- properties of a dog
+  * @name: string variable
+  * @age: float variable
+  * @owner: string variable
+  *
+  * Description: defines the name, age and owner of a dog
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
