af6c9652ed323e9cfe38418ecb9be28f66ec9856
RhysMurage <rhysmwangi96@gmail.com>
2022-04-20 08:26:03 -0400

    Fix non-integer error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,18 +15,21 @@ int main(int argc, char *argv[])
 	long sum = 0;
 	long number = 0;
 	char error[] = "Error";
+	char *ptr = argv[counter];
 
 	while (argc >= 2)
 	{
 		number = strtol(argv[counter], NULL, 10);
-		if (number == 0)
+		/* check that ASCII Code are for integers only */
+		if ((*ptr < 48) | (*ptr > 57))
 		{
 			printf("%s\n", error);
 			return (0);
-		}
+		}	
 		sum += number;
 		++counter;
 		--argc;
+		ptr++;
 	}
 	printf("%ld\n", sum);
 	return (0);
