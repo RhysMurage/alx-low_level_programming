77978f3c338ee16593b55e041b3057b5f1381f7e
RhysMurage <rhysmwangi96@gmail.com>
2022-05-03 14:09:43 -0400

    Fix error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -36,7 +36,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		if (write(fd, text_content, _strlen(text_content)) == 1)
+		if (write(fd, text_content, _strlen(text_content)) == -1)
 			return (-1);
 	}
 
