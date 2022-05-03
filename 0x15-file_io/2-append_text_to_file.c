d225682fc1e4bfa33cbf8bd55ca9b14e60c6dad4
RhysMurage <rhysmwangi96@gmail.com>
2022-05-03 14:16:46 -0400

    Fix error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int _strlen(char *s)
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
