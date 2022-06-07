b479653b26f27b93aa7a4d80c40c636733631323
RhysMurage <rhysmwangi96@gmail.com>
2022-06-07 11:42:40 -0400

    Remove '/'

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 	{
 		d = malloc(sizeof(struct dog));
-		printf("%s", "Name: (/nil)");
+		printf("%s", "Name: (nil)");
 	}
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
