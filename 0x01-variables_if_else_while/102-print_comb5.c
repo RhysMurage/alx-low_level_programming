0f63ca15bf75df7b5f683b1a7f1fd24bc223522f
RhysMwangi <rhys.mwangi@lctafrica.net>
2025-06-17 18:30:00 +0300

    Fix incorrect output

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,25 +9,28 @@
  * Return: 0
  */
 
-int main(void) {
+int main() {
     int ft,fo, st, so;
 
     for (ft=48; ft < 58; ft++){
         for(fo=48; fo < 58; fo++) {
             for (st=48; st < 58; st++){
-                for (so=48; so < 58; so++) {
-                    putchar(ft);
-                    putchar(fo);
-                    putchar(32);
-                    putchar(st);
-                    putchar(so);
-                    if (st == 57 && so == 57)
+                for (so=fo; so < 58; so++) {
+                    if (ft==st && fo==so)
                         continue;
                     else {
-                        putchar(44);
+                        putchar(ft);
+                        putchar(fo);
                         putchar(32);
+                        putchar(st);
+                        putchar(so);
+                        if (fo==57 && fo==56 && st == 57 && so == 57)
+                            continue;
+                        else {
+                            putchar(44);
+                            putchar(32);
+                        }
                     }
-                    
                 }
             }
         }
